dfece951137be461382dc8550f90c2f5114ce863
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-05-01 17:47:16 +0300

    docs: add JSDoc comments for init function in navbar.js

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,4 +1,13 @@
 const Navbar = (function () {
+    /**
+     * Initializes the navbar toggler functionality.
+     * Sets up an event listener on the specified button to toggle the
+     * visibility of the specified element.
+     *
+     * @param {string} buttonID - The ID of the button element that triggers the toggle.
+     * @param {string} elementID - The ID of the content element to be shown or hidden.
+     */
+
     function init(buttonID, elementID) {
         const togglerButton = document.querySelector(`#${buttonID}`);
         const elementContent = document.querySelector(`#${elementID}`);
