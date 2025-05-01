87647a019eb3c2b6bbce190426aee08b1e270174
Khaled Mohamed <k.mohamed.11.98@gmail.com>
2025-05-01 17:44:06 +0300

    docs: add JSDoc comments for clearAllInputs & redirectToPage functions in helper.js

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,10 +1,20 @@
 const Helper = (function () {
+    /**
+     * Clears the value of all input elements passed as arguments.
+     * @param {...HTMLInputElement} arr - A rest parameter that accepts multiple input elements.
+     */
+
     function clearAllInputs(...arr) {
         arr.forEach(input => {
             input.value = "";
         });
     }
 
+    /**
+     * Redirects to a specified page after a given amount of time.
+     * @param {string} pageName - The name of the page to redirect to.
+     * @param {number} [time=4000] - The amount of time to wait before redirecting in ms.
+     */
     function redirectToPage(pageName, time = 4000) {
         setTimeout(() => {
             window.location.href = `${pageName}`;
